21985feb271821eda9fadfe42a82028e4c470f24
modmuss50 <m123world@gmail.com>
2015-02-14 23:29:16 +0000

    added some basic code

---
+++ PACKCREATOR/makePack.py
@@ -0,0 +1,44 @@
+__author__ = 'modmuss50'
+
+import sys
+import os
+
+#(PACKNAME) (PACKMAKER) (PACKDESCRIPTION) (MCVER) (FORGEVER)
+
+print "Starting pack generator"
+packName = ""
+packCreator = ""
+packDescription = ""
+mcVersion = ""
+forgeVersion = ""
+
+packName = sys.argv[1]
+packCreator = sys.argv[2]
+packDescription = sys.argv[3]
+mcVersion = sys.argv[4]
+forgeVersion = sys.argv[5]
+
+filename = "packs/" + packName
+
+if os.path.isdir(filename):
+    print "That pack exits! I cannot continue!"
+    sys.exit(1)
+else:
+    os.makedirs(filename)
+
+file = open(filename + "/desc.txt", "w")
+file.write(packDescription)
+file.close()
+
+file = open(filename + "/mcInfo.txt", "w")
+file.write(mcVersion + "-" + forgeVersion)
+file.close()
+
+file = open(filename + "/packInfo.txt", "w")
+file.write(packName + "\n")
+file.write(packCreator)
+file.close()
+
+file = open(filename + "/mods.txt", "w")
+file.close()
+
